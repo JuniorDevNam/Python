75f58218248cb410ff11b08f3f405a09d2e4da37
IAmNamNguyen <138494161+JuniorDevNam@users.noreply.github.com>
2024-08-21 17:53:28 +0700

    Create Câu 17.py

---
+++ Lớp 12/BÀI TOÁN/Câu 17.py
@@ -0,0 +1,5 @@
+'''
+Bài 17: Viết chương trình Python chèn một số m (m nhập vào từ bàn phím )
+vào đầu danh sách, cuối danh sách và vị trí thứ 5 của danh sách.
+'''
+a=[2, -4, 1, 9, -3, 6, 3, -2, 6, 8]
