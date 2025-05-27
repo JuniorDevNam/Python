8df461d95a80069a29ade67bd9d2c0c849eab4ff
JuniorDevNam <138494161+JuniorDevNam@users.noreply.github.com>
2025-05-27 08:27:43 +0700

    Update muasam.py

--- Lớp 12/HSG TP HN - THPT - 2025/Bài 2 (done)/muasam.py
+++ Lớp 12/HSG TP HN - THPT - 2025/Bài 2 (done)/muasam.py
@@ -1,4 +1,8 @@
-
+import sys, os
+input_file = os.path.join(sys.path[0], "muasam.INP")
+output_file = os.path.join(sys.path[0], "muasam.OUT")
+sys.stdin = open(input_file, 'r')
+sys.stdout = open(output_file, 'w')
 N, L, R = map(int, input().split())
 A = list(map(int, input().split()))
 A.sort()
