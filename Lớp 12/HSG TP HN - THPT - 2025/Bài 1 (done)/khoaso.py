744eacb895bba2285a337c39f8f2f56a1eb24c66
JuniorDevNam <138494161+JuniorDevNam@users.noreply.github.com>
2025-05-27 08:20:08 +0700

    Update khoaso.py

--- Lớp 12/HSG TP HN - THPT - 2025/Bài 1 (done)/khoaso.py
+++ Lớp 12/HSG TP HN - THPT - 2025/Bài 1 (done)/khoaso.py
@@ -1,7 +1,8 @@
 import os, sys
 input_file = os.path.join(sys.path[0],"khoaso.INP")
+output_file = os.path.join(sys.path[0], "khoaso.OUT")
 sys.stdin = open(input_file, 'r')
-
+sys.stdout = open(output_file, 'w')
 khoaso = list(map(int, input().split()))
 password = (2,0,2,5)
 count = 0
