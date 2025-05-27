59fc4a9e78b86cbdefe075ddb84595fec29f763f
JuniorDevNam <138494161+JuniorDevNam@users.noreply.github.com>
2025-05-27 08:24:00 +0700

    Update khoaso.py

--- Lớp 12/HSG TP HN - THPT - 2025/Bài 1 (done)/khoaso.py
+++ Lớp 12/HSG TP HN - THPT - 2025/Bài 1 (done)/khoaso.py
@@ -7,11 +7,5 @@ khoaso = list(map(int, input().split()))
 password = (2,0,2,5)
 count = 0
 for i in range(len(khoaso)):
-    if khoaso[i] == password[0]:
-        count += 0
-    else:
-        if khoaso[i] <= 5:
-            count += abs(khoaso[i] - password[i])
-        else:
-            count += (10 - abs(khoaso[i] - password[i]))
+    count += min(abs(khoaso[i] - password[i]), 10 - abs(khoaso[i] - password[i]))
 print(count)
